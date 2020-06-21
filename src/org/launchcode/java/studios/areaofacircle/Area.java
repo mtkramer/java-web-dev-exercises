6d39fc5c44dce6d2175221fbb7cccb745f3b5fd4
mtkramer <kramerm1@mail.usf.edu>
2020-06-21 13:06:51 -0400

    Studio 1 bonus missions completed

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -4,12 +4,21 @@ import java.util.Scanner;
 
 public class Area {
     public static void main(String[] args){
-        double radius;
+        double radius = 0.0;
 
         Scanner input = new Scanner(System.in);
-            System.out.println("What is the radius? ");
-            radius = input.nextDouble();
-            System.out.println("The area of that circle is " + (Math.PI * radius * radius));
+            while(radius <= 0.0){
+                System.out.println("Please enter circle radius greater than 0.0");
+                try{
+                    radius = input.nextDouble();
+                }
+                catch(Exception e){
+                    System.out.println("Your entry for radius was invalid due to " + e);
+                    System.out.println("Only numbers are valid for radius");
+                    System.exit(1);
+                }
+            }
+            System.out.println("The area of that circle is " + Circle.getArea(radius));
         input.close();
     }
 }
