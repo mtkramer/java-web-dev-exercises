7e022782c0541db63ca09e309f46d6f283ea845a
mtkramer <kramerm1@mail.usf.edu>
2020-06-24 15:02:16 -0400

    added constructors and getters setters for Student

--- src/exercises/class3/Student.java
+++ src/exercises/class3/Student.java
@@ -1,13 +1,46 @@
 package exercises.class3;
 
-// Start working here with your Student class.
-// To instantiate the Student class, add your code to the main in the file, SchoolPractice.
-
 public class Student {
 
     private String name;
     private int studentId;
-    private int numberOfCredits = 0;
-    private double gpa = 0.0;
+    private int numberOfCredits;
+    private double gpa;
+
+    public Student(String name, int studentId, int numberOfCredits, double gpa){
+        this.name = name;
+        this.studentId = studentId;
+        this.numberOfCredits = numberOfCredits;
+        this.gpa = gpa;
+    }
+    public Student(String name, int studentId){
+        this(name, studentId, 0,0.0);
+    }
+
+    public String getName(){
+        return this.name;
+    }
+    public int getStudentId(){
+        return this.studentId;
+    }
+    public int getNumberOfCredits(){
+        return this.numberOfCredits;
+    }
+    public double getGpa(){
+        return this.gpa;
+    }
+
+    public void setName(String aName){
+        this.name = aName;
+    }
+    public void setStudentId(int aStudentId){
+        this.studentId = aStudentId;
+    }
+    public void setNumberOfCredits(int aNumberOfCredits){
+        this.numberOfCredits = aNumberOfCredits;
+    }
+    private void setGPA(double aGpa){
+        this.gpa = aGpa;
+    }
 
 }
