7914c466748a3411fe93195257311d44b71c9b92
mtkramer <kramerm1@mail.usf.edu>
2020-06-30 15:35:50 -0400

    Completed getGradeLevel() and addGrade methods

--- src/exercises/class3/Student.java
+++ src/exercises/class3/Student.java
@@ -2,7 +2,7 @@ package exercises.class3;
 
 public class Student {
 
-    public String name;
+    private String name;
     private int studentId;
     private int numberOfCredits;
     private double gpa;
@@ -36,8 +36,27 @@ public class Student {
     public void setNumberOfCredits(int aNumberOfCredits){
         this.numberOfCredits = aNumberOfCredits;
     }
-    private void setGPA(double aGpa){
-        this.gpa = aGpa;
+    private void setGpa(double aGpa){this.gpa = aGpa;}
+
+    public String getGradeLevel() {
+        if(this.numberOfCredits < 0){
+            return "ERROR: numberOfCredits is negative";
+        } else if (this.numberOfCredits < 30){
+            return "Freshman";
+        } else if (this.numberOfCredits < 60){
+            return "Sophomore";
+        } else if (this.numberOfCredits < 90){
+            return "Junior";
+        } else {
+            return "Senior";
+        }
+    }
+
+    public void addGrade(int courseCredits, double grade){
+        double totalQualityScore = this.gpa * this.numberOfCredits;
+        totalQualityScore += courseCredits * grade;
+        setNumberOfCredits(this.numberOfCredits + courseCredits);
+        setGpa(totalQualityScore / this.numberOfCredits);
     }
 
 }
